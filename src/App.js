eeebef2c0c677a742fdebb847331a2d6679b3784
Samir Benzada <samirbenzada@gmail.com>
2019-10-25 22:10:00 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import { UserContext } from './UserContext';
 
 import IwashNavBar from './js/components/Navbar/Navbar';
 import NotFound from './js/components/NotFound/NotFound';
@@ -9,16 +10,23 @@ const Home = lazy(() => import('./js/views/Home/Home'));
 const Login = lazy(() => import('./js/views/Login/Login'));
 
 function App() {
+  const [user, setUser] = useState(null);
+
+
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-      <IwashNavBar/>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route exact path="/" component={Home} />
-          <Route render={() => <NotFound/>} />
-        </Switch>
-      </Suspense>
+      <UserContext.Provider value={providerValue}>
+        <Suspense fallback={<div>Loading...</div>}>
+        <IwashNavBar/>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route exact path="/" component={Home} />
+            <Route render={() => <NotFound/>} />
+          </Switch>
+        </Suspense>
+      </UserContext.Provider>
     </Router>
   );
 }
--- src/js/views/Login/Login.js
+++ src/js/views/Login/Login.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
 
 import coin from '../../../img/coin.png';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <section className={styles.section}>
             
                 <img className={styles.coin} src={coin} alt="coin"/>
                 <br/>
-                <input className={styles.inputForm} type="text" placeholder="Email" />
+                <input 
+                    className={styles.inputForm} 
+                    type="text" 
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
                 <br/>
-                <input className={styles.inputForm} type="text" placeholder="Password" />
+                <input 
+                    className={styles.inputForm} 
+                    type="password" 
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <br/>
                 <button className={["btn",styles.buttLogin].join(' ')}>Login</button>
                 <br/>
