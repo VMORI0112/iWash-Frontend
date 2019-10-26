954d222a26e43cec5d66490672573f51b4c8956e
Samir Benzada <samirbenzada@gmail.com>
2019-10-25 22:55:34 -0400

    update

--- src/js/views/Register/Register.js
+++ src/js/views/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import styles from './Register.module.css';
 
@@ -6,25 +6,26 @@ import coin from '../../../img/coin.png';
 
 const Register = () => {
     let history = useHistory();
+    const inputFname = useRef(null);
+    const inputLname = useRef(null);
+    const inputEmail = useRef(null);
+    const inputPass = useRef(null);
 
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [msg1, setMsg1] = useState('');
-    const [msg2, setMsg2] = useState('');
-    const [msg3, setMsg3] = useState('');
-    const [msg4, setMsg4] = useState('');
+
 
     const registerFormHandler = () => {
         if (!firstname) {
-            setMsg1("Enter your First Name!")
+            inputFname.current.style.border = "1px solid red";
         } if (!lastname) {
-            setMsg2("Enter your Last Name!")
+            inputLname.current.style.border = "1px solid red";
         } if (!email) {
-            setMsg3("Enter your Email!") 
+            inputEmail.current.style.border = "1px solid red";
         } if (!password) {
-            setMsg4("Enter your Password!") 
+            inputPass.current.style.border = "1px solid red";
         } else {
 
             let userRegisterData = JSON.stringify({
@@ -57,11 +58,11 @@ const Register = () => {
             
                 <img className={styles.coin} src={coin} alt="coin"/>
                 <br/>
-                <small>{msg1}</small>
                 <input 
                     className={styles.inputForm} 
                     type="text" 
                     placeholder="First Name"
+                    ref={inputFname}
                     value={firstname}
                     onChange={(e) => setFirstname(e.target.value)}
                 />
@@ -70,16 +71,16 @@ const Register = () => {
                     className={styles.inputForm} 
                     type="text" 
                     placeholder="Last Name"
+                    ref={inputLname}
                     value={lastname}
                     onChange={(e) => setLastname(e.target.value)}
                 />
-                <small>{msg2}</small>
                 <br/>
-                <small>{msg3}</small>
                 <input 
                     className={styles.inputForm} 
                     type="email" 
                     placeholder="Email"
+                    ref={inputEmail}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
@@ -88,10 +89,10 @@ const Register = () => {
                     className={styles.inputForm} 
                     type="password" 
                     placeholder="Password"
+                    ref={inputPass}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <small>{msg4}</small>
                 <br/>
 
                 <button 
