5463cb838f79b2a4ae3f9364db133552ef6b422f
Samir Benzada <samirbenzada@gmail.com>
2019-10-26 12:07:06 -0400

    update

--- src/js/views/Login/Login.js
+++ src/js/views/Login/Login.js
@@ -1,13 +1,44 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import styles from './Login.module.css';
 
 import coin from '../../../img/coin.png';
 
 const Login = () => {
-
+    let history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const loginFormHandler = () => {
+        let userLoginData = JSON.stringify({
+            email: email,
+            password: password
+        });
+        fetch('http://0.0.0.0:3000/login', {
+            method: 'POST',
+            body: userLoginData,
+            headers:{
+                'Content-Type': 'application/json'
+            }
+            }).then(res => res.json())
+            .then(response => {
+                let token = response.token;
+                let email = response.user.email;
+                let firstname = response.user.first_name;
+                let lastname = response.user.last_name;
+                if (!token && !email && !firstname && !lastname ) {
+                    alert("Sorry we couldn't find an account with that email.\n\n Try to register first.")
+                } else {
+                    alert('LOGIN SUCCESSFUL');
+                    localStorage.setItem('token', token)
+                    localStorage.setItem('email', email);
+                    localStorage.setItem('firstname', firstname);
+                    localStorage.setItem('lastname', lastname);
+                    history.push('/');
+                }
+            })
+            .catch(error => alert('Error:', error));
+    }
 
     return (
         <section className={styles.section}>
@@ -30,7 +61,14 @@ const Login = () => {
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 <br/>
-                <button className={["btn",styles.buttLogin].join(' ')}>Login</button>
+                
+                <button 
+                    className={["btn",styles.buttLogin].join(' ')}
+                    onClick={loginFormHandler}
+                >
+                    Login
+                </button>
+
                 <br/>
                 <br/>
                 <Link to="/" className={styles.forgotP}>
