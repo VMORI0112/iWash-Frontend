9f20d7e0578aabdfca05dbd38a7aa1606832f949
samir benzada <samirbenzada@gmail.com>
2019-10-26 12:33:00 -0400

    update

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -4,6 +4,11 @@ import styles from './Navbar.module.css';
 import coin from '../../../img/coin.png';
 
 const IwashNavBar = () => {
+
+    const firstname = localStorage.getItem('firstname');
+    const lastname = localStorage.getItem('lastname');
+    const token = localStorage.getItem('token');
+
     return (
         <>
         <nav className={["navbar fixed-top navbar-expand-lg navbar-light", styles.navBarI].join(' ')}>
@@ -18,14 +23,22 @@ const IwashNavBar = () => {
                     {/* <li><a href="//">Home</a></li>
                     <li><a href="//">Page 1</a></li>
                     <li><a href="//">Page 2</a></li> */}
-                    <li>
-                        <Link to="/login" className={["btn", styles.btnGreen].join(' ')}> 
-                            Login
+                    {!token && !firstname && !lastname ?
+                        <li>
+                            <Link to="/login" className={["btn", styles.btnGreen].join(' ')}> 
+                                Login
+                            </Link>
+                            <Link to="/register" className={["btn", styles.btnGreen].join(' ')}> 
+                                Register
+                            </Link>
+                        </li>
+                    : 
+                        <li>
+                        <Link to="/login"> 
+                            {firstname} {lastname}
                         </Link>
-                        <Link to="/register" className={["btn", styles.btnGreen].join(' ')}> 
-                            Register
-                        </Link>
-                    </li>
+                </li>
+                    }
                 </ul>
             </div>
         </nav>
