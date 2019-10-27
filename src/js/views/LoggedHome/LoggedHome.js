6b2d147d700b283d6f98e6991c0afda84b7c88ff
Samir Benzada <samirbenzada@gmail.com>
2019-10-27 13:18:41 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -5,28 +5,73 @@ import washerClose from '../../../img/washing/w2.png';
 
 const LoggedHome = () => {
     return (
+        <>
         <section className={styles.section}>
-            <h1>Logged Home</h1>
+            <h1 className="text-center">Laundromat</h1>
             <div className="container">
+                <h2 className="text-center">Wahers</h2>
                 <div className="row">
 
-                    <div className="col">
+                    <div className="col-4 text-center">
                         <img className={styles.available} src={washerOpen} alt="washer" />
                     </div>
-                    <div className="col">
+                    <div className="col-4 text-center">
                         <img className={styles.noavailable} src={washerClose} alt="washer" />
                     </div>
-                    <div className="col">
+                    <div className="col-4 text-center">
                         <img className={styles.available} src={washerOpen} alt="washer" />
                     </div>
-                    <div className="col">
-                        <img className={styles.noavailable} src={washerClose} alt="washer" />
-                    </div>
+                    
+                </div>
+                <div className="row mt-5">
 
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.noavailable} src={washerClose} alt="washer" />
+                    </div>
+                    
+                </div>
+                
+                
+            </div>
+        </section>
+        <section className={styles.section2}>
+            <div className="container">
+            <h2 className="text-center">Dryers</h2>
+                <div className="row" style={{backgroundColor: "white"}}>
+
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.noavailable} src={washerClose} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    
+                </div>
+                <div className="row mt-5">
+
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.available} src={washerOpen} alt="washer" />
+                    </div>
+                    <div className="col-4 text-center">
+                        <img className={styles.noavailable} src={washerClose} alt="washer" />
+                    </div>
                     
                 </div>
             </div>
         </section>
+        </>
     );
 };
 
