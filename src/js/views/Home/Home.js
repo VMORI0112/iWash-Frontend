b787b8bd75d06e337b28df2fb53fd613b7bc2984
samir benzada <samirbenzada@gmail.com>
2019-10-28 10:28:17 -0400

    update

--- src/js/views/Home/Home.js
+++ src/js/views/Home/Home.js
@@ -5,7 +5,14 @@ const Home = () => {
     return (
         <section className={styles.section}>
             <div className={["text-center", styles.main].join(' ')}>
-                <h1>Welcome</h1>
+                <div class={styles.bannerInfo}>
+                    <div class="container">
+                        <div class="style-banner text-center">
+                            <p class="text-li mb-2">Wash, Dry &amp; Fold</p>
+                            <h3 class="text-wh font-weight-bold text-uppercase">We Are Expert Cleaner</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
