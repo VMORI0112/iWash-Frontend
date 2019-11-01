b753270f47c9b14583163ed542cd81466ab1b1c7
samir benzada <samirbenzada@gmail.com>
2019-11-01 10:13:37 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -23,138 +23,50 @@ const LoggedHome = () => {
                 </div>
 
                 <div className="row">
-                {!washersData ? 'loading' : washersData.map((item,index) => {
-                    return (
-                        <div key={index} className="col-3 my-3">
-                            <Link to={"machine/"+index} >
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="card-text">
-                                            <h4 className="text-center" >{item.name}</h4>
-                                            <span className="badge badge-success">Available</span>
-                                            <span className="badge badge-pill badge-primary float-right">{item.number}</span>
+                    {!washersData ? 'loading' : washersData.map((item,index) => {
+                        return (
+                            <div key={index} className="col-3 my-3">
+                                <Link to={"machine/"+index} >
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <div className="card-text">
+                                                <h4 className="text-center" >{item.name}</h4>
+                                                <span className="badge badge-success">Available</span>
+                                                <span className={["badge badge-pill badge-primary float-right", styles.number].join(' ')}>{item.number}</span>
+                                            </div>
                                         </div>
+                                        <img src={washerClose} className="card-img-top" alt="..." />
                                     </div>
-                                    <img src={washerClose} className="card-img-top" alt="..." />
-                                </div>
-                            </Link>
-                        </div>
-                    )
-                })}
+                                </Link>
+                            </div>
+                        )
+                    })}
+                </div>
+                <br/>
+                <hr/><br/>
 
-                    
-
+                <div className={["row", styles.dryerContainer].join(' ')}>
+                    {!dryersData ? 'loading' : dryersData.map((item,index) => {
+                        return (
+                            <div key={index} className="col-3 my-3">
+                                <Link to={"machine/"+index} >
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <div className="card-text">
+                                                <h4 className="text-center" >{item.name}</h4>
+                                                <span className="badge badge-success">Available</span>
+                                                <span className={["badge badge-pill badge-primary float-right", styles.number].join(' ')}>{item.number}</span>
+                                            </div>
+                                        </div>
+                                        <img src={washerClose} className="card-img-top" alt="..." />
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })}
                 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-                {/* <div className="row">
-
-                    <div className="col-4 text-right p-3">
-                        <Link to="/machine/wash1" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-right p-3">
-                        <img className={styles.noavailable} src={washerClose} alt="washer" />
-                    </div>
-                    <div className="col-4 text-right p-3">
-                        <Link to="/machine/wash2" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    
-                </div> */}
-                {/* <div className="row mt-5">
-
-                    <div className="col-4 text-right p-3">
-                        <Link to="/machine/wash3" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-right p-3">
-                        <Link to="/machine/wash4" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-right p-3">
-                        <img className={styles.noavailable} src={washerClose} alt="washer" />
-                    </div>
-                    
-                </div> */}
-                
-                
-            {/* </div>
-        </section>
-        <section className={styles.section2}>
-            <div className="container">
-            <h2 className="text-center">Dryers</h2>
-                <div className="row" style={{backgroundColor: "white"}}>
-
-                    <div className="col-4 text-center">
-                        <Link to="/machine/dry1" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-center">
-                        <img className={styles.noavailable} src={washerClose} alt="washer" />
-                    </div>
-                    <div className="col-4 text-center">
-                        <Link to="/machine/dry2" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    
-                </div>
-                <div className="row mt-5">
-
-                    <div className="col-4 text-center">
-                        <Link to="/machine/dry3" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-center">
-                        <Link to="/machine/dry4" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    <div className="col-4 text-center">
-                        <Link to="/machine/dry5" className={styles.machinChoice}>
-                            <img className={styles.available} src={washerOpen} alt="washer" />
-                        </Link>
-                    </div>
-                    
-                </div> */}
-            </div>
-
-            {!washersData ? 'loading' : washersData.map((item,index) => {
-            return (
-                <Link to={"machine/"+index} key={index}>
-                    {item.type} - {item.number}
-                    <br/>
-                </Link>
-            )
-        })}
-        <br/>
-        {!dryersData ? 'loading' : dryersData.map((item,index) => {
-            return (
-                <Link to={"machine/"+index} key={index}>
-                    {item.type} - {item.number}
-                    <br/>
-                </Link>
-            )
-        })}
-
+            </div>    
         </section>
         </>
     );
