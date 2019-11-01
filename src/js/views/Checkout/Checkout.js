d8078c145d4c694ddbe21cefb97f94538d417247
samir benzada <samirbenzada@gmail.com>
2019-10-31 22:09:50 -0400

    update

---
+++ src/js/views/Checkout/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Checkout.module.css';
+
+const Checkout = () => {
+    return (
+        <section className={styles.section}>
+            Checkout page
+        </section>
+    );
+};
+
+export default Checkout;
