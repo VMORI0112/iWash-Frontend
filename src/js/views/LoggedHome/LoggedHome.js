6e278b268aac4b64327d7c01ee7d5aa39c2539f6
samir benzada <samirbenzada@gmail.com>
2019-11-01 12:04:10 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { UserContext } from '../../../UserContext';
 
 import styles from './LoggedHome.module.css';
-// import washerOpen from '../../../img/washing/w1.png';
+import dryerClose from '../../../img/washing/dry.png';
 import washerClose from '../../../img/washing/w2.png';
 
 
@@ -37,7 +37,7 @@ const LoggedHome = () => {
                 <div className={["row", washerContainer].join(' ')}>
                     {!washersData ? 'loading' : washersData.map((item,index) => {
                         return (
-                            <div key={index} className="col-3 my-3">
+                            <div key={index} className={["col-3 my-3", styles.cardContainer].join(' ')}>
                                 <Link className={styles.links} to={"machine/"+index} >
                                     <div className="card">
                                         <div className="card-body">
@@ -58,8 +58,8 @@ const LoggedHome = () => {
                 <div className={["row", dryerContainer].join(' ')}>
                     {!dryersData ? 'loading' : dryersData.map((item,index) => {
                         return (
-                            <div key={index} className="col-3 my-3">
-                                <Link to={"machine/"+index} >
+                            <div key={index} className={["col-3 my-3", styles.cardContainer].join(' ')}>
+                                <Link className={styles.links} to={"machine/"+index} >
                                     <div className="card">
                                         <div className="card-body">
                                             <div className="card-text">
@@ -68,7 +68,7 @@ const LoggedHome = () => {
                                                 <span className={["badge badge-pill badge-primary float-right", styles.number].join(' ')}>{item.number}</span>
                                             </div>
                                         </div>
-                                        <img src={washerClose} className="card-img-top" alt="..." />
+                                        <img src={dryerClose} className="card-img-top" alt="..." />
                                     </div>
                                 </Link>
                             </div>
