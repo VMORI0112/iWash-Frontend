15d3d469862b03762158bd6acdac494e529ee212
samir benzada <samirbenzada@gmail.com>
2019-11-03 11:44:35 -0500

    update

--- src/js/views/Wallet/Wallet.js
+++ src/js/views/Wallet/Wallet.js
@@ -7,16 +7,11 @@ const Wallet = () => {
             <div className={["container text-center", styles.white].join(' ')}>
                 <h1>Wallet</h1>
                 <p>your current credit is: $40</p>
-                <button className="btn btn-danger m-3" > + $10</button>
-                <button className="btn btn-danger m-3" > + $20</button>
-                <button className="btn btn-danger m-3" > + $30</button>
-                <button className="btn btn-danger m-3" > + $40</button>
-                <button className="btn btn-danger m-3" > + $50</button>
-                <button className="btn btn-danger m-3" > + $60</button>
-                <button className="btn btn-danger m-3" > + $70</button>
-                <button className="btn btn-danger m-3" > + $80</button>
-                <button className="btn btn-danger m-3" > + $90</button>
-                <button className="btn btn-danger m-3" > + $100</button>
+                <button className={["btn btn-lg m-3", styles.btnOrange].join(' ')} > + $10</button>
+                <button className="btn btn-danger btn-lg m-3" > + $20</button>
+                <button className="btn btn-danger btn-lg m-3" > + $30</button>
+                <button className="btn btn-danger btn-lg m-3" > + $40</button>
+                <button className="btn btn-danger btn-lg m-3" > + $50</button>
             </div>
         </section>
     );
