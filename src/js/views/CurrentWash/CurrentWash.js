688492c689b5dc28fba2df2b4e395fb22ddef7d2
samir benzada <samirbenzada@gmail.com>
2019-11-07 17:39:20 -0500

    update

--- src/js/views/CurrentWash/CurrentWash.js
+++ src/js/views/CurrentWash/CurrentWash.js
@@ -7,18 +7,23 @@ const CurrentWash = () => {
 
     const {currentWashing} = useContext(UserContext); 
 
+    console.log(currentWashing);
+
     return (
         <section className={styles.section}>
             <div className="container">
                 <h2 className="text-center">your current wash</h2>
                 {!currentWashing ? 'Loading...' : currentWashing.map((item,index) => {
                     return (
-                        <div key={index} className="alert alert-primary" >
-                            <h4>{item.cicle} || {item.time}min || #{item.machineNumber}</h4>
-                            start: {item.start} - End: {item.end}
-                            {item.time}
-                            <br/>
-                            <Timer start={item.start} end={item.end} />
+                        <div key={index} className={["alert", styles.alertBlue].join(' ')} >
+                            <div className="row text-center">
+                                <div className={["col", styles.cycle].join(' ')}> <span role="img" aria-label="dd">💧</span> {item.cicle}</div>
+                                <div className={["col", styles.time].join(' ')}> <span role="img" aria-label="dd">⏱️</span> {item.time}min</div>
+                                <div className={["col", styles.number].join(' ')}> <span role="img" aria-label="ds">#️⃣</span> {item.machineNumber}</div>
+                            </div>
+                            <div className="text-center mt-3">
+                                <Timer start={item.start} end={item.end} />
+                            </div>
                             <br/>
                         </div>
                     )
