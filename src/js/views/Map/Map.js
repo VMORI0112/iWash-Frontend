95c2d5f9404df1364eb5c8a53480436545c98c0f
Samir Benzada <samirbenzada@gmail.com>
2019-11-11 15:24:46 -0500

    update

--- src/js/views/Map/Map.js
+++ src/js/views/Map/Map.js
@@ -7,9 +7,11 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const styles = {
-    width: "100vw",
-    height: "calc(100vh - 80px)",
-    position: "absolute"
+    width: "80%",
+    height: "50%",
+    position: "absolute",
+    left: "10%",
+    top: "20%"
   };
 
 const Map = () => {
@@ -44,13 +46,10 @@ const Map = () => {
 
     return (
         <section className={mystyles.section} style={{minHeight: windowHeight}}>
-        map here
-        <br/>
+        
         lat: {latitude} - Long: {longitude} - t: {timestamp} - acc: {accuracy} - error: {error}
 
-        {/* <div className="container p-2">
-            <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114937.54832567982!2d-80.21018223341697!3d25.81022472015342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a6172bfeddb9%3A0x37be1741259463eb!2sMiami%20Beach%2C%20FL!5e0!3m2!1sen!2sus!4v1572315179765!5m2!1sen!2sus" width="100%" height="300"></iframe>
-		</div> */}
+        <h3 className="text-center">Laundromat Uses iWash App Arround You</h3>
 
         <div ref={el => (mapContainer.current = el)} style={styles} />
 
