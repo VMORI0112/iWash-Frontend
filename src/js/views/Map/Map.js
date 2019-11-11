27d9d409f18d9bd7e59ce73703895ae8f6363be9
samir benzada <samirbenzada@gmail.com>
2019-11-10 21:27:53 -0500

    update

--- src/js/views/Map/Map.js
+++ src/js/views/Map/Map.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../../UserContext';
+import { usePosition } from 'use-position';
 import styles from './Map.module.css';
 
 const Map = () => {
 
     const {windowHeight} = useContext(UserContext);
+    const { latitude, longitude, timestamp, accuracy, error } = usePosition();
 
     // console.log(navigator.geolocation);
 
@@ -12,6 +14,7 @@ const Map = () => {
         <section className={styles.section} style={{minHeight: windowHeight}}>
         map here
         <br/>
+        lat: {latitude} - Long: {longitude} - t: {timestamp} - acc: {accuracy} - error: {error}
         </section>
     );
 };
