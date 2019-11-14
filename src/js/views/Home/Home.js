631ba36e65dc8d841f91716291d8f458223634a1
samir benzada <samirbenzada@gmail.com>
2019-11-14 10:47:23 -0500

    update

--- src/js/views/Home/Home.js
+++ src/js/views/Home/Home.js
@@ -5,6 +5,8 @@ import styles from './Home.module.css';
 import fer from '../../../img/home/fer.png';
 import lav from '../../../img/home/lav.png';
 import linge from '../../../img/home/linge.png';
+import apple from '../../../img/home/apple.png';
+import google from '../../../img/home/google.png';
 
 const Home = () => {
     return (
@@ -15,7 +17,7 @@ const Home = () => {
                     <div className="container">
                         <div className="style-banner text-center">
                             <p className="text-li mb-2">Wash, Dry &amp; Fold</p>
-                            <h3 className="text-wh font-weight-bold text-uppercase">We Are Expert Cleaners</h3>
+                            <h3 className="text-wh font-weight-bold text-uppercase"><i className={["fas fa-info align-top", styles.i].join(' ')}></i>Wash Smart Laundry</h3>
                         </div>
                     </div>
                 </div>
@@ -91,6 +93,15 @@ const Home = () => {
             <div className="container p-2 my-5">
             <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114937.54832567982!2d-80.21018223341697!3d25.81022472015342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a6172bfeddb9%3A0x37be1741259463eb!2sMiami%20Beach%2C%20FL!5e0!3m2!1sen!2sus!4v1572315179765!5m2!1sen!2sus" width="100%" height="300"></iframe>
 			</div>
+
+            <div className="row mb-5">
+                <div className="col text-right">
+                    <a href="/"><img src={apple} alt="apple" /></a>
+                </div>
+                <div className="col text-left">
+                    <a href="/"><img src={google} alt="google" /></a>
+                </div>
+            </div>
 
         </section>
     );
