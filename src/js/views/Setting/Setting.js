5fd3f8a4a7648307802e4c577a22e1674b404352
samir benzada <samirbenzada@gmail.com>
2019-11-14 09:55:41 -0500

    update

--- src/js/views/Setting/Setting.js
+++ src/js/views/Setting/Setting.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Setting.module.css';
+import geeks from '../../../img/presentation/4geeks.png';
 
 const Setting = () => {
     return (
@@ -10,7 +11,7 @@ const Setting = () => {
                     <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-user"></i>&nbsp;Update Profile</a>
                     <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-hand-holding-usd"></i>&nbsp;My Wallet</a>
                     <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-history"></i>&nbsp;My Washing History</a>
-                    <a href="#" class="list-group-item list-group-item-action">&nbsp;Back to Presentation</a>
+                    <a href="#" class="list-group-item list-group-item-action"> <img src={geeks} alt="4geeks" width="30px" /> &nbsp;Back to Presentation</a>
                 </div>
             </div>
         </section>
