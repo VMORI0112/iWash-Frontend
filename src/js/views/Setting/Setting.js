6c9f18ff5767f42887cbaa9eaee830baced49527
samir benzada <samirbenzada@gmail.com>
2019-11-14 09:51:45 -0500

    update

--- src/js/views/Setting/Setting.js
+++ src/js/views/Setting/Setting.js
@@ -5,6 +5,14 @@ const Setting = () => {
     return (
         <section className={styles.section}>
             <h1>Setting</h1>
+            <div className="container">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-user"></i>&nbsp;Update Profile</a>
+                    <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-hand-holding-usd"></i>&nbsp;My Wallet</a>
+                    <a href="#" class="list-group-item list-group-item-action"><i class="fas fa-history"></i>&nbsp;My Washing History</a>
+                    <a href="#" class="list-group-item list-group-item-action">&nbsp;Back to Presentation</a>
+                </div>
+            </div>
         </section>
     );
 };
